3bd6d4ac137c2d863fcb01feb1cf961376978c93
Saúl Valdelvira <saul@saulv.es>
2024-06-29 00:16:51 +0200

    Add bench for standard OnceLock

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,7 +3,7 @@ extern crate test;
 use test::Bencher;
 
 use delay_init::delay;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::OnceLock};
 
 const N: usize = 10000;
 
@@ -33,6 +33,25 @@ fn normal(b: &mut Bencher) {
         for _ in 0..N {
             assert_eq!(*map.get(&12).unwrap(), 12);
             assert!(map.get(&11).is_none());
+        }
+    });
+}
+
+fn get_map() -> &'static HashMap<i32,i32> {
+    static CELL: OnceLock<HashMap<i32,i32>> = OnceLock::new();
+    CELL.get_or_init(|| {
+        let mut map = HashMap::new();
+        map.insert(12,12);
+        map
+    })
+}
+
+#[bench]
+fn stdcell(b: &mut Bencher) {
+    b.iter(|| {
+        for _ in 0..N {
+            assert_eq!(*get_map().get(&12).unwrap(), 12);
+            assert!(get_map().get(&11).is_none());
         }
     });
 }
